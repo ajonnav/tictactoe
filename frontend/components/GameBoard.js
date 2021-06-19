4bbcdbfadbc72ab5ec65263f72d32ba612143a9e
Ani Jonnavithula <ajonnav@gmail.com>
2021-06-19 10:58:15 -0700

    add auth following supabase tutorial, add basic tic tac toe board

---
+++ frontend/components/GameBoard.js
@@ -0,0 +1,15 @@
+export default function GameBoard() {
+  return (
+    <div id="board">
+      <div class="box" id="box-1"></div>
+      <div class="box" id="box-2"></div>
+      <div class="box" id="box-3"></div>
+      <div class="box" id="box-4"></div>
+      <div class="box" id="box-5"></div>
+      <div class="box" id="box-6"></div>
+      <div class="box" id="box-7"></div>
+      <div class="box" id="box-8"></div>
+      <div class="box" id="box-9"></div>
+    </div>
+  )
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,22 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabaseClient'
+import Auth from '../components/Auth'
+import GameBoard from '../components/GameBoard'
 
-export default HomePage
+export default function Home() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <GameBoard/>}
+    </div>
+  )
+}
