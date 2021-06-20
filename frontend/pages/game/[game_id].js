b24d68c6875fc1c526eb0b55f27c21f79da9f2c2
Ani Jonnavithula <ajonnav@gmail.com>
2021-06-20 12:07:45 -0700

    subscribe to changes

--- frontend/pages/game/[game_id].js
+++ frontend/pages/game/[game_id].js
@@ -1,12 +1,22 @@
 import { useRouter } from 'next/router'
 import GameBoard from '../../components/GameBoard'
 import { supabase } from '../../utils/supabaseClient'
+import { useState } from 'react';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Game = ({current_state}) => {
+  const [currState, setCurrState] = useState(current_state);
   const router = useRouter()
   const { game_id } = router.query
+
+  const games = supabase
+    .from(`games:id=eq.${game_id}`)
+    .on('*', payload => {
+      const { new: { current_state: newCurrState } } = payload;
+      setCurrState(newCurrState);
+    })
+    .subscribe()
 
   const onEmptyCellClickHandler = async (event) => {
     event.preventDefault();
@@ -22,19 +32,16 @@ const Game = ({current_state}) => {
     });
     const result = await res.json();
     if(res.status !== 200) {
+      // TODO: manage this better.
       alert(`ERROR: ${result.error}`);
       console.log(`Error: ${result}`);
-    } else {
-      console.log(`Success! ${res.data}`);
     }
   }
-  return <GameBoard currState={current_state} onEmptyCellClickHandler={onEmptyCellClickHandler}/>;
+
+  return <GameBoard currState={currState} onEmptyCellClickHandler={onEmptyCellClickHandler}/>;
 }
 
 export async function getServerSideProps(context) {
-  const currState = {
-    tiles: ['x','o','x','','','','','',''],
-  }
   const { game_id } = context.params;
   let { data: games, error } = await supabase
     .from('games')
