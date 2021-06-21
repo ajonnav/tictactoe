b9e946a1624251570bce18685a42e3a602fe50ba
Ani Jonnavithula <ajonnav@gmail.com>
2021-06-21 06:51:06 -0700

    fix deploy

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,14 +1,16 @@
 import Link from 'next/link'
 import { supabase } from '../utils/supabaseClient'
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
   const user = supabase.auth.user()
+  const router = useRouter()
   return (
     <div>
       <Link href="/"><button>Home</button></Link>
       <Link href="/stats"><button>Stats</button></Link>
       <button className="button block" onClick={() => supabase.auth.signOut()}>Sign Out</button>
-      <div>You are: {user.id}</div>
+      { user && <div>You are: {user.id}</div>}
     </div>
   )
 }
